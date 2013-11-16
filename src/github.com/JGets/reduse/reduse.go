0ec8b20e2ad18675ec248287753413cfc548fb6e
Jack Gettings <JGets@me.com>
2013-11-16 11:58:54 -0500

    updated configs, modularized code

--- src/github.com/JGets/reduse/reduse.go
+++ src/github.com/JGets/reduse/reduse.go
@@ -4,9 +4,9 @@ import(
 	"os"
 	"html/template"
 	"log"
-	"errors"
-	"bufio"
-	"strings"
+	// "errors"
+	// "bufio"
+	// "strings"
 	
 	"github.com/hoisie/web"			/* http://webgo.io */
 )
@@ -82,114 +82,7 @@ func error404(ctx *web.Context, url string){
 				 				   })
 }
 
-func startup() (bool, error){
-	
-	//TODO
-	//get the server address / base url from user config file
-	
-	configs, err := parseConfigFile()
-	
-	if err != nil {
-		return false, err
-	}
-	
-	configs, err = populateDefaults(configs)
-	
-	if err != nil {
-		return false, err
-	}
-	
-	err = checkConfigs(configs)
-	if err != nil {
-		return false, err
-	}
-	
-	
-	setConfigVars(configs)
-	
-	return true, nil
-}
 
-func parseConfigFile() (map[string]string, error){
-	//Open and read in the config file
-	
-	file, err := os.Open(CONFIG_FILE_NAME)
-	if err != nil {
-		return nil, errors.New("Error: unable to open config file '" + CONFIG_FILE_NAME + "'")
-	}
-	defer file.Close()
-	
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	
-	if scanner.Err() != nil {
-		return nil, scanner.Err()
-	}
-	
-	//parse the lines of the config file & put them into a map[string]string
-	ret := make(map[string]string)
-	
-	for _, val := range lines {
-		if val != "" {
-			spl := strings.Split(val, "=")
-			if len(spl) == 2 {
-				//Parse the config value
-				ret[spl[0]] = spl[1]
-			} else if len(spl) == 1 {
-				//no config value used, so initialize with an empty string
-				ret[spl[0]] = ""
-			} else {
-				logger.Printf("Warning: Could not parse '%v' from config file. (Too many arguments?)\n", val)
-			}
-		}
-	}
-	
-	return ret, nil
-}
-
-
-func populateDefaults(configs map[string]string) (map[string]string, error){
-	
-	//TO DO
-	// if configs["server_address"] == "" {
-	// 	configs["server_address"] = DEFAULT_SERVER_ADDRESS
-	// }
-	
-	for k, v := range configs {
-		if v == "" {
-			def, inDef := DEFAULTS[k]
-			if inDef {
-				configs[k] = def
-			} else {
-				logger.Printf("Warning: Empty config key '%v' with no default value, ignoring\n", k)
-			}
-		}
-	}
-	
-	
-	return configs, nil
-}
-
-
-func checkConfigs(configs map[string]string) error {
-	
-	if !strings.HasSuffix(configs["base_url"], "/") {
-		return errors.New("Error: config base url '" + configs["base_url"] + "' does not end with a backslash '/'")
-	}
-	
-	
-	return nil
-	
-}
-
-func setConfigVars(configs map[string]string) {
-	appName = configs["app_name"]
-	serverAddress = configs["server_address"]
-	siteBaseURL = configs["base_url"]
-}
 
 
 
